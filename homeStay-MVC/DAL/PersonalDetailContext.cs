129f06e3f5f49afb57f11a7571455daa6ab747b1
Pratik Kabade <76637730+pratikkabade@users.noreply.github.com>
2022-05-26 00:10:26 +0530

    Removed PersonalContext
    To reduce the clutter

---
+++ homeStay-MVC/DAL/PersonalDetailContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.Entity;
+using homeStay_MVC.Models;
+
+namespace homeStay_MVC.DAL
+{
+    public class PersonalDetailContext : DbContext
+    {
+        public PersonalDetailContext() : base("HomeContext")
+        { }
+
+        public DbSet<Owner> Owners { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
